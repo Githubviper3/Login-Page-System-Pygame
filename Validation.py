46609c8712d0ce75b49c3738bf142c140bc1933e
Githubviper3 <117672877+Githubviper3@users.noreply.github.com>
2024-02-12 14:58:54 +0000

    Update Validation.py

--- Validation.py
+++ Validation.py
@@ -1,30 +1,34 @@
 def Lencheck(password):
-  return [True,True] if len(password) >= 8 else [False, "Too Short"]
+ return [True,True] if len(password) >= 8 else [False, "Must be 8 or more Characters Long"]
+
 
 def Uppercheck(password):
-  return [True,True] if any(i.isupper() for i in password) else [False, "No Uppercase Letters"]
+ return [True,True] if any(i.isupper() for i in password) else [False, "No Uppercase Letters"]
+
 
 def Lowercheck(password):
-  return [True,True] if any(i.islower() for i in password) else [False, "No Lowercase Letters"]
+ return [True,True] if any(i.islower() for i in password) else [False, "No Lowercase Letters"]
+
 
 def SpcCheck(password):
-  return [True,True] if password.isalnum() is False else [False, "No Special Characters"]
+ return [True,True] if password.isalnum() is False else [False, "No Special Characters"]
+
 
 def Numcheck(password):
-  return [True,True] if any(i.isdigit() for i in password) else [False, "No Numbers"]
+ return [True,True] if any(i.isdigit() for i in password) else [False, "No Numbers"]
+
 
 def paswordcheck(password):
-  checks1= [Lencheck(password)[0],Uppercheck(password)[0],Lowercheck(password)[0],SpcCheck(password)[0],Numcheck(password)[0]]
-  checks1 = all(checks1)
-  checks2= [Lencheck(password)[1], Uppercheck(password)[1], Lowercheck(password)[1], SpcCheck(password)[1], Numcheck(password)[1]]
-  endmessage = "Invalid: "
-  for i in checks2:
-    if isinstance(i,str):
-      endmessage = endmessage + i + ","
-
-  if checks1:
-    return [True,True]
-  else:
-    return [False,endmessage[:-1]]
+ checks1= [Lencheck(password)[0],Uppercheck(password)[0],Lowercheck(password)[0],SpcCheck(password)[0],Numcheck(password)[0]]
+ checks1 = all(checks1)
+ checks2= [Lencheck(password)[1], Uppercheck(password)[1], Lowercheck(password)[1], SpcCheck(password)[1], Numcheck(password)[1]]
+ endmessage = ""
+ for i in checks2:
+   if isinstance(i,str):
+     endmessage = endmessage + i + "\n"
 
 
+ if checks1:
+   return [True,"True"]
+ else:
+   return [False,endmessage[:-1]]
